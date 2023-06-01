e537b5f71d47a7e67ea3b1d31cefb11dc137abf6
Renata Kuroda <renatakuroda@gmail.com>
2023-06-01 20:55:25 +1000

    sets up project including supabase

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+import supabase from './config/supabaseClient'
+
+function App() {
+  console.log(supabase)
+  return (
+    <div>
+      <h2>My Pet Care</h2>
+
+    </div>
+  )
+   
+}
+
+export default App
